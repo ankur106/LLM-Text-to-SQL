b7489adbbe95cb07409aab8586f8e55a396cbb52
Ankur Patel <ankurnpatel106@gmail.com>
2024-08-23 10:05:21 -0700

    Basic SQL Query Chain

---
+++ Chain.py
@@ -0,0 +1,55 @@
+from langchain_community.utilities import SQLDatabase;
+from langchain.chains import create_sql_query_chain
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+import os
+
+from operator import itemgetter
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+load_dotenv()
+uri = f"postgresql://{os.environ['POSTGRES_USERNAME']}:{os.environ['POSTGRES_PASSWORD']}@localhost/LLM_SQL"
+
+db = SQLDatabase.from_uri(uri)    
+
+# print(db.dialect)
+# print(db.get_usable_table_names())
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+execute_query = QuerySQLDataBaseTool(db=db)
+write_query = create_sql_query_chain(llm, db)
+chain = write_query | execute_query
+# response = chain.invoke({"question": "Is there a state table?"})
+# print(response)
+
+
+
+
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """
+)
+
+
+answer = answer_prompt | llm | StrOutputParser()
+chain = (
+    RunnablePassthrough.assign(query=write_query).assign(
+        result=itemgetter("query") | execute_query
+    )
+    | answer
+)
+
+response  = chain.invoke({"question": "give me schema of the database?"})
+
+print(response)
+
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-3.5-turbo-16k")
+
+from langchain_core.messages import HumanMessage, SystemMessage
+
+messages = [
+    SystemMessage(content="Translate the following from English into Gujarati"),
+    HumanMessage(content="You are dick"),
+]
+
+print(model.invoke(messages))
